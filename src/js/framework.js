50c1513471b6d724ac2ad29bb7ba60ce4a9a55ae
Jo Dickson <jogreybill@gmail.com>
2017-02-09 10:11:28 -0600

    Updated jquery version; added tether.js for total bootstrap compatibility

--- src/js/framework.js
+++ src/js/framework.js
@@ -2,6 +2,8 @@
 // Import vendor assets
 //
 
+//=require ../../bower_components/isInViewport/lib/isInViewport.js
+//=require ../../bower_components/tether/dist/js/tether.min.js
 //=require ../../bower_components/bootstrap/dist/js/bootstrap.js
 
 
