4f3ed8c1a2107fde0b1ac1a83d19fdc20d5748e1
Jo Dickson <jogreybill@gmail.com>
2017-02-22 10:03:18 -0600

    Removed tether and (currently) unused inViewport lib

--- src/js/framework.js
+++ src/js/framework.js
@@ -2,8 +2,6 @@
 // Import vendor assets
 //
 
-//=require ../../bower_components/isInViewport/lib/isInViewport.js
-//=require ../../bower_components/tether/dist/js/tether.min.js
 //=require ../../bower_components/bootstrap/dist/js/bootstrap.js
 
 
