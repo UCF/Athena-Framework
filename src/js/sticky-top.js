e758d4367a4ac071fb7d552a41440b831175fa2c
Jo Dickson <jogreybill@gmail.com>
2017-04-19 10:06:59 -0500

    Applied polyfill for .sticky-top utility class

---
+++ src/js/sticky-top.js
@@ -0,0 +1,18 @@
+//
+// Applies a position: sticky polyfill to .sticky-top elements.
+//
+
+(function ($) {
+
+  $.fn.stickyTop = function () {
+
+    // Let the plugin determine whether polyfill logic is required
+    this.Stickyfill();
+
+    return this;
+
+  };
+
+  $('.sticky-top').stickyTop();
+
+}(jQuery));
