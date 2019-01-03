8d3b78df19891f5f6cc14bf565394cf210fb8442
Jo Dickson <jogreybill@gmail.com>
2019-01-03 11:19:11 -0500

    Removed redundant inline no-sync toggling around gulp-config.json fetch

--- gulpfile.js
+++ gulpfile.js
@@ -73,12 +73,10 @@ let config = {
   docBaseURL: '/docs-local'
 };
 
-/* eslint-disable no-sync */
 if (fs.existsSync('./gulp-config.json')) {
   const overrides = JSON.parse(fs.readFileSync('./gulp-config.json'));
   config = merge(config, overrides);
 }
-/* eslint-enable no-sync */
 
 
 //
