26eb605419d8e67ed6e3e119d04e9dbda33edce8
Jo Dickson <jogreybill@gmail.com>
2020-02-19 12:30:06 -0500

    Replaced Prism with highlightjs; fixing various code highlighting-related things while breaking others

--- _docs/.eleventy-prod.js
+++ _docs/.eleventy-prod.js
@@ -1,12 +1,17 @@
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const MarkdownIt = require('markdown-it');
-const md = new MarkdownIt();
+const md = new MarkdownIt({
+  html: true
+}).disable('code');
 const Entities = require('html-entities').XmlEntities;
 const entities = new Entities();
-const Prism = require('prismjs');
+const hljs = require('highlight.js');
+
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
+  //
+  // Eleventy configuration
+  //
+  eleventyConfig.setLibrary('md', md);
 
   eleventyConfig.setLiquidOptions({
     root: [
@@ -18,25 +23,64 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('LICENSE');
   eleventyConfig.addPassthroughCopy('static');
 
+  //
+  // Shortcode | Callout (Alert)
+  //
   eleventyConfig.addPairedShortcode('callout', function (content, callout_type) {
     // Render content as markdown first; otherwise
-    // it gets parsed as HTML and markdown isn't translated
+    // it gets parsed as HTML and markdown isn't translated.
+    // https://www.11ty.dev/docs/languages/markdown/#why-cant-i-return-markdown-from-paired-shortcodes-to-use-in-a-markdown-file
     content = entities.decode(md.render(content));
     return `<div class="afd-callout afd-callout-${callout_type}">${content}</div>`;
   });
 
+  //
+  // Shortcode | Highlighted Code
+  //
+  eleventyConfig.addPairedShortcode('highlight', function (content, language) {
+    // Markdown has a bad time with multiple linebreaks between HTML.
+    // Force a &NewLine; entity between new lines to ensure Markdown
+    // doesn't choke:
+    highlighted = content.replace(/\n\n/g, '\n&NewLine;\n');
+
+    // Perform syntax highlighting on code sample
+    highlighted = hljs.highlight(language, highlighted).value
+
+    // Now that our transformed HTML has been succesfully highlighted,
+    // remove our manual newline entity insertions in the code sample:
+    highlighted = highlighted.replace(/&amp;NewLine;/g, '');
+
+    return `<div class="highlight"><pre><code>${highlighted}</code></pre></div>`;
+  });
+
+  //
+  // Shortcode | Code Example
+  //
   eleventyConfig.addPairedShortcode('example', function (content) {
-    code = entities.decode(md.renderInline(content));
-    highlighted = Prism.highlight(content, Prism.languages.markup, 'html');
+    // Markdown has a bad time with multiple linebreaks between HTML.
+    // Force a &NewLine; entity between new lines to ensure Markdown
+    // doesn't choke:
+    highlighted = content.replace(/\n\n/g, '\n&NewLine;\n');
+
+    // Perform syntax highlighting on code sample
+    highlighted = hljs.highlight('html', entities.decode(highlighted)).value;
+
+    // Now that our transformed HTML has been succesfully highlighted,
+    // remove our manual newline entity insertions in the code sample:
+    highlighted = highlighted.replace(/&amp;NewLine;/g, '');
+
     return `<div class="afd-example">${content}</div><div class="highlight"><pre><code>${highlighted}</code></pre></div>`;
   });
 
+  //
+  // Build options
+  //
   return {
     pathPrefix: '/Athena-Framework',
     dir: {
       input: './', // Equivalent to Jekyll's source property
       output: '../docs', // Equivalent to Jekyll's destination property
-      layouts: '_layouts',
+      layouts: '_layouts'
     }
   };
 };
